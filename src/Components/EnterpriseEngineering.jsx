fefa9e3dfe423dd4a111926fdfbb839002a16e7c
sonam-123-indvibe <88430627+sonam-123-indvibe@users.noreply.github.com>
2025-05-21 13:06:20 +0530

    Update EnterpriseEngineering.jsx

--- src/Components/EnterpriseEngineering.jsx
+++ src/Components/EnterpriseEngineering.jsx
@@ -55,6 +55,7 @@ const EnterpriseEngineering = () => {
             >
               {tech.icon}
               <p className='small_font' >{tech.title}</p>
+              <p>Hellow</p>
             </div>
              
              </>
