00e79899fd22218115e2c2a8cc36faa6a20fc658
sonam-123-indvibe <88430627+sonam-123-indvibe@users.noreply.github.com>
2025-05-21 12:57:42 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,11 @@ const Footer = () => {
 
           {/* Company Info and Main Links */}
           <div className="col-md-4 mb-4">
-            <h2 className="text-info">INDVIBE INFOTECH PVT LTD</h2>
+          <h2 className="fw-bold">
+  <span className="text-info">IndVibe</span><span className="text-white"> Infotech</span>
+</h2>
+
+
             
           </div>
 
