ee9f66daf0447c1d4f558c4f6b583b8725a1ade1
sonam-123-indvibe <88430627+sonam-123-indvibe@users.noreply.github.com>
2025-05-21 13:03:44 +0530

    Update EnterpriseEngineering.jsx

--- src/Components/EnterpriseEngineering.jsx
+++ src/Components/EnterpriseEngineering.jsx
@@ -15,24 +15,24 @@ import { DiPhonegap } from "react-icons/di";
 import '../assets/style/EnterpriseEngineering.css';
 
 const techList = [
-  { icon: <FaAndroid />, color: '#3DDC84' },
-  { icon: <FaApple />, color: '#000' },
-  { icon: <FaReact />, color: '#61DBFB' },
-  { icon: <FaJava />, color: '#f89820' },
-  { icon: <FaNodeJs />, color: '#3C873A' },
-  { icon: <FaPython />, color: '#3776AB' },
-  { icon: <FaCloud />, color: '#00BFFF' },
-  { icon: <FaDatabase />, color: '#6c63ff' },
-  { icon: <FaAws />, color: '#FF9900' },          // AWS Orange
-  { icon: <FaBootstrap />, color: '#bf8fff' },    // Bootstrap Purple
-  { icon: <FaVuejs />, color: '#42b883' },        // Vue Green
-  { icon: <FaHtml5 />, color: '#E34F26' },        // HTML5 Orange
-  { icon: <FaCss3Alt />, color: '#1572B6' },      // CSS3 Blue
-  { icon: <FaAngular />, color: '#DD0031' },      // Angular Red
-  { icon: <SiMongodb />, color: '#47A248' },      // MongoDB Green
-  { icon: <FaFlutter />, color: '#4ca4ed' },      // Flutter Blue
-  { icon: <TbBrandKotlin />, color: '#ad68f7' },  // Kotlin Purple
-  { icon: <DiPhonegap />, color: '#e8d7fc' },
+  { icon: <FaAndroid />,title:"Android" ,color: '#3DDC84' },
+  { icon: <FaApple />,title:"Apple" ,color: '#000' },
+  { icon: <FaReact />,title:"React" ,color: '#61DBFB' },
+  { icon: <FaJava />,title:"Java" ,color: '#f89820' },
+  { icon: <FaNodeJs />,title:"Node" ,color: '#3C873A' },
+  { icon: <FaPython />,title:"Python" ,color: '#3776AB' },
+  { icon: <FaCloud />,title:"Cloud" ,color: '#00BFFF' },
+  { icon: <FaDatabase />,title:"Data Base" ,color: '#6c63ff' },
+  { icon: <FaAws />,title:"AWS" ,color: '#FF9900' },          // AWS Orange
+  { icon: <FaBootstrap />,title:"Bootstrap" ,color: '#bf8fff' },    // Bootstrap Purple
+  { icon: <FaVuejs />,title:"Vue" ,color: '#42b883' },        // Vue Green
+  { icon: <FaHtml5 />,title:"HTML" ,color: '#E34F26' },        // HTML5 Orange
+  { icon: <FaCss3Alt />,title:"CSS" ,color: '#1572B6' },      // CSS3 Blue
+  { icon: <FaAngular />,title:"Angular" ,color: '#DD0031' },      // Angular Red
+  { icon: <SiMongodb />,title:"MongoDB" ,color: '#47A248' },      // MongoDB Green
+  { icon: <FaFlutter />,title:"Flutter" ,color: '#4ca4ed' },      // Flutter Blue
+  { icon: <TbBrandKotlin />,title:"Kotlin" ,color: '#ad68f7' },  // Kotlin Purple
+  { icon: <DiPhonegap />,title:"Phonegap" ,color: '#e8d7fc' },
  
 ];
 // <FaBootstrap /> <FaVuejs /> <FaHtml5 /> <FaCss3Alt />
@@ -47,14 +47,20 @@ const EnterpriseEngineering = () => {
 
         <div className="icon-grid">
           {techList.map((tech, index) => (
+            <>
             <div
-              className="tech-icon-box"
+              className="tech-icon-box d-block"
               key={index}
               style={{ color: tech.color }}
             >
               {tech.icon}
+              <p className='small_font' >{tech.title}</p>
             </div>
+             
+             </>
           ))}
+
+          
         </div>
       </div>
     </section>
